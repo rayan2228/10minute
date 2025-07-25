601a6636ca58d613ab3d70a292aa1af8bd911c90
rayan2228 <rayanh.dev@gmail.com>
2025-07-25 22:43:47 +0600

    feat: implement middleware for locale detection and redirection

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextResponse } from "next/server";
+
+const locales = ["en", "bn"];
+
+// Get the preferred locale, similar to the above or using a library
+function getLocale(request) {
+  const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
+  const headers = { "accept-language": acceptedLanguage };
+  const languages = new Negotiator({ headers }).languages();
+  const defaultLocale = "bn";
+
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
